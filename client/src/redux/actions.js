3815aeb37eb9aa3073086dfd1d643319a53fee93
Jonnier <jonniersalaverria@gmail.com>
2023-09-02 16:38:44 -0500

    All, ByName y ByID listo

--- client/src/redux/actions.js
+++ client/src/redux/actions.js
@@ -1,20 +1,19 @@
 import axios from "axios";
 
 
-import { GET_GAMES, GET_BY_NAME, API_KEY } from "./types";
+import { GET_GAMES, GET_BY_NAME, GET_BY_ID} from "./types";
 
 
 export const getGames = ()=>{//`https://api.rawg.io/api/games?key=${API_KEY}`
     return async dispatch =>{
        const response= await axios.get(`http://localhost:3001/videogames`);
-
+        
        return dispatch({
         type: GET_GAMES,
         payload :response.data
        })
     }
 }
-
 
 
 export const getGameByName = (name)=>{
@@ -26,4 +25,20 @@ export const getGameByName = (name)=>{
        })
     }
 }
+
+
+export const getGameById = (id) =>{
+    return async (dispatch) => {
+        try {
+          const response = await axios.get(`http://localhost:3001/videogames/${id}`);
+          dispatch({
+            type: GET_BY_ID,
+            payload: response.data,
+        });
+        
+        } catch (error) {
+          console.error(error);
+        }
+      };
+};
 
