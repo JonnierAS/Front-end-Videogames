4fc6dd6df4f64954d881cb9e7a6c55fe9940d984
Jonnier <jonniersalaverria@gmail.com>
2023-09-04 20:03:48 -0500

    Traslado de info a Results

--- client/src/views/Home/Home.jsx
+++ client/src/views/Home/Home.jsx
@@ -1,62 +1,45 @@
 import { useEffect, useState} from "react";
-import {useNavigate} from "react-router-dom"
 import {useDispatch, useSelector} from "react-redux"
 import {getGameByName, getGames} from "../../redux/actions"
 
+
 import Navbar from "../../components/Navbar/Navbar";
-import Cards from "../../components/Cards/Cards";
 import "./home.style.css";
+import Loading from "../../components/Loading/Loading";
+import Results from "./Results/Results";
 
 
 const Home = ()=>{
-
     const dispatch = useDispatch();
-    const games = useSelector((state)=>state.allGames);
-    
-    const navigate = useNavigate();
+    const allGames = useSelector((state)=>state.allGames); 
     const [searchString, setSearchString] = useState("");
-   
+    
+    //Filtro con la backend
     const handleChange = (event)=>{
+        event.preventDefault()
         setSearchString(event.target.value)
             
     }; 
-    //Filtro con la backend
-
     const handleSubmit = (event)=>{
         event.preventDefault()
         dispatch(getGameByName(searchString))
     }
     
-
-
-    // filtro sobre el estado
-    // const [filtered, setFiltered] = useState(games);
-    // const [searchString, setSearchString] = useState("");
-
-    // const handleChange = (event)=>{
-    //     // event.preventDefault()
-    //     setSearchString(event.target.value)
-    // };
-
-    // const handleSubmit=()=>{
-    //     event.preventDefault();
-    //     const filteredGame = games.filter(game => game.name.includes(searchString))
-    //     setFiltered(filteredGame)//modifica el stado local con los games que coincidieron
-    // }
-   
     useEffect(()=>{
         dispatch(getGames());
-    /* return(()=>{  //SI entramos al detalle de un juego y nos salimos no queremos que se guarde
-        crearDetail()  //para limpiar el estado cuando nos salgamos de la pagina
-    }) */
-    },[dispatch])
+    },[])
 
     return(
+        <>
+        {allGames ? (
         <div className="home">
-            <h2 className='homeTitle' onClick={() => navigate("/")}>JasosPlay</h2>
             <Navbar handleChange={handleChange} handleSubmit={handleSubmit} />
-            <Cards games={games} />
+            <Results /> 
         </div>
+        ) : (
+            <Loading />
+          )}
+        </>
     )
 };
 
