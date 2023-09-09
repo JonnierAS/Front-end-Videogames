edbcd041b838c27da09c24b9f66d62fca3f62f96
Jonnier <jonniersalaverria@gmail.com>
2023-09-09 15:35:47 -0500

    Landing responsive mobile

--- client/src/views/Landing/LandingPage.jsx
+++ client/src/views/Landing/LandingPage.jsx
@@ -1,23 +1,23 @@
-import "./Landing.css";
+import style from "./Landing.module.css";
 import img from "../../../img/videogame.png";
 import img2 from "../../../img/bola.png";
 import { NavLink } from "react-router-dom";
 
 const LandingPage = () => {
   return (
-      <div className="main_container_Landing">
-        <span className="login_circuit-mask"></span>
-        <span className="login_circuit-mask2"></span>
-      <img className="img" src={img} alt="landing" />
-      <img className="img3" src={img2} alt="landing" />
-      <div className="bodi_landin">
-        <div className="login">
-          <h1 className="titles">Let'sPlay</h1>
+      <div className={style.main_container_Landing}>
+        <span className={style.login_circuit_mask} />
+        <span className={style.login_circuit_mask2}/>
+      <img className={style.img} src={img} alt="landing" />
+      <img className={style.img3} src={img2} alt="landing" />
+      <div className={style.bodi_landin}>
+        <div className={style.login}>
+          <h1 className={style.titles}>Let'sPlay</h1>
           <br />
           {/* <h1 className="login_heading">Iniciar sesion</h1>   */}
-          <div className="containbtn">
+          <div className={style.containbtn}>
           <NavLink  to={"/home"}>
-            <button  className="btn">START</button>
+            <button  className={style.btn}>START</button>
           </NavLink></div>
         </div>
       </div>
