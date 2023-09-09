a6dbb14d107f853e20b05fa52addaa0c8ea00e1d
Jonnier <jonniersalaverria@gmail.com>
2023-09-09 13:45:17 -0500

    Result listo

--- client/src/views/Home/Results/Results.jsx
+++ client/src/views/Home/Results/Results.jsx
@@ -1,18 +1,19 @@
-import { useSelector, useDispatch } from "react-redux";
-import { useState, useEffect } from "react";
+import { useSelector } from "react-redux";
+import { useState } from "react";
 
 import Cards from "../../../components/Cards/Cards";
 import Pagination from "../Pagination/Pagination";
 import Loading from "../../../components/Loading/Loading";
 
+
 const Results = () => {
-  const dispatch = useDispatch();
 
   const allGames = useSelector((state) => state.allGames);
-  const allGenres = useSelector((state) => state.allGenres);
 
   const numberOfCards = 15;
   const [page, setPage] = useState(1);
+  
+
 
   const totalPages = Math.ceil(allGames.length / numberOfCards);
   const startPage = (page - 1) * numberOfCards;
