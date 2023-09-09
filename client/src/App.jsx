8546edc456adfab7563c6e9df9e813a1f7d4de36
Jonnier <jonniersalaverria@gmail.com>
2023-09-09 12:24:53 -0500

    Componente completo

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,24 +1,28 @@
-import { Routes, Route} from "react-router-dom";
-
+import { Routes, Route, useLocation} from "react-router-dom";
 
 import LandingPage from './views/Landing/LandingPage';
 import Home from './views/Home/Home';
-import Navbar from "./components/Navbar/Navbar";
 import Create from "./views/Create/Create"
 import Detail from "./views/Detail/Detail"
 import Footer from "./views/Footer/Footer";
+import NotFound from "./views/Home/NotFound/NotFound";
 
 
 function App() {
+  
+
+  
+
   return (
     <div className="App">
-
+      
       <Routes>
         <Route exact path="/" element={<LandingPage />} />
-        <Route path="/" element={<Navbar />} />
+        {/* <Route path="/" element={<Navbar />} /> */}
         <Route exact path='/home' element={<Home />} />
         <Route exact path='/create' element={<Create />} />
         <Route path='/game/:id' element={<Detail />} />
+        <Route path='/notFound' element={<NotFound />} />
         <Route path="/" element={<Footer />} />
       </Routes>
     </div>
