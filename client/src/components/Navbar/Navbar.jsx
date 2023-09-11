1b85813462965f303d7bfa3ff9c2aa03a73b471d
Jonnier <jonniersalaverria@gmail.com>
2023-09-11 09:52:44 -0500

    correcion del search

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -17,18 +17,18 @@ const Navbar = ({handleChange, handleSubmit})=>{
             </div>
             {/* <Filter /> */}
              <div className={styles.searMain}>
-                  <form onClick={handleSubmit}>
+                  <form onSubmit={handleSubmit}>
                      <input onChange={handleChange} className={styles.searchBox} placeholder="Search a game" type="text" />
-                     <span  type="submit" >
+                     <button onSubmit={handleSubmit} type="submit"  >
                      <svg 
                      xmlns="http://www.w3.org/2000/svg" 
                      height="1em" 
                      viewBox="0 0 512 512"
                      className={styles.search}
                      >
-                    <path 
+                    <path fill="currentColor"
                     d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z"/>
-                    </svg></span>
+                    </svg></button>
                  </form>
              </div>
           </div>
