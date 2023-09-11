f8d5da41fbb8f6b136fd975548205b544e97c0c8
Jonnier <jonniersalaverria@gmail.com>
2023-09-11 10:50:37 -0500

    sin cambios relevantes

--- client/src/views/Home/Home.jsx
+++ client/src/views/Home/Home.jsx
@@ -1,20 +1,25 @@
 import { useEffect, useState} from "react";
 import {useDispatch, useSelector} from "react-redux"
-import {getGameByName, getGames} from "../../redux/actions"
+import {getGameByName, getGames, getGenres} from "../../redux/actions"
 
 
 import Navbar from "../../components/Navbar/Navbar";
+import Footer from "../Footer/Footer";
 import "./home.style.css";
 import Loading from "../../components/Loading/Loading";
 import Results from "./Results/Results";
+import Filter from "./Filter/Filter";
 
 
 const Home = ()=>{
     const dispatch = useDispatch();
     const allGames = useSelector((state)=>state.allGames); 
+
     const [searchString, setSearchString] = useState("");
-    
-    //Filtro con la backend
+
+      
+
+    //Filtro con el backend
     const handleChange = (event)=>{
         event.preventDefault()
         setSearchString(event.target.value)
@@ -26,15 +31,19 @@ const Home = ()=>{
     }
     
     useEffect(()=>{
-        dispatch(getGames());
-    },[])
+        if(allGames.length === 0){
+            dispatch(getGames())
+        }
+    },[])//como esta vacio solo se ejecuta una vez
 
     return(
         <>
         {allGames ? (
         <div className="home">
             <Navbar handleChange={handleChange} handleSubmit={handleSubmit} />
-            <Results /> 
+            <Filter />
+            <Results />
+            <Footer /> 
         </div>
         ) : (
             <Loading />
