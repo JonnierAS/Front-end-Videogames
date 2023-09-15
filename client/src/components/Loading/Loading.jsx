460d5252b69ad27fc37de29b4a26d37a31c203f4
Jonnier <jonniersalaverria@gmail.com>
2023-09-15 09:18:49 -0500

    img

--- client/src/components/Loading/Loading.jsx
+++ client/src/components/Loading/Loading.jsx
@@ -1,11 +1,11 @@
 import styles from './Loading.module.css'
-
+import img from "../../../img/loading.gif"
 const Loading = () => {
   return (
     <div className={styles.main_container}>
       <div className={styles.wait_sign_container}>
         <h2 className={styles.text}>Wait while we bring back our data...</h2>
-        <img src="../../../img/loading.gif" alt="gif" className={styles.spinner}/>
+        <img src={img} alt="gif" className={styles.spinner}/>
       </div>
     </div>
   )
